04887d9364878a187c04f644241db0fd3b20c4ca
R. G. Esteves <rodolfo.g.esteves@intel.com>
2020-06-16 12:47:30 -0700

    Start using llvm core components

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 
+#include "llvm/IR/LLVMContext.h"
+
+using namespace llvm;
+
 int main()
 {
-    using namespace std;
-    cout << "Hello, world!" << endl;
+    LLVMContext context;
+    std::cout << "Hello, changed world (now with llvm)!" << std::endl;
     return EXIT_SUCCESS;
 }
